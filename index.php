aabf67d6c358845b795a7e524bc1b9c4a37d9d4b
George Rabadi <96255554+GeorgeRabadi@users.noreply.github.com>
2023-02-16 15:58:31 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <? php
-header('Location: https://www.google.com/');
-exit;
-?>
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?> 
