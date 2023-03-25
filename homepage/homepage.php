bdc015e57073fb777315c11d7889c96121919560
SouloCider <luke.wittmayer@knights.ucf.edu>
2023-03-25 02:29:21 -0400

    Home Page Update
    Added to the home page and updated the logo

---
+++ homepage/homepage.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+    <link rel="stylesheet" href="../style.css">
+</head>
+<body>
+    <div class="navbar">
+        <a class="active" href="../registration/register.php">Home</a>
+        <a href="../registration/login.php">Login</a>
+        <a href="../registration/register.php">Register</a>
+    </div>
+
+    <div class= "container">
+        <div class="title"><img src = "../imgs/ucf.png"></div>
+        <img id="statue_img" src = "../imgs/statue.png">
+        <div class="home">
+            <span style="font-family:fangsong">Welcome</span>
+        </div>
+    </div>
+
+</body>
+</html>
